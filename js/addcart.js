dbdbe04c99b9a051e619076848d57b1cb2e7382c
Taynara Sene <tpereira@tray.net.br>
2016-03-21 09:12:20 -0300

    Componente addcart

---
+++ js/addcart.js
@@ -0,0 +1,16 @@
+function addCart(dataProductId){
+     var dataSession = $("html").attr("data-session");
+   $.ajax({
+     method: "POST",
+     url: "/web_api/cart/",
+     contentType: "application/json; charset=utf-8",
+     data: '{"Cart":{"session_id":"'+dataSession+'","product_id":"'+dataProductId+'","quantity":"1"}}'
+   }).done(function( response, textStatus, jqXHR ) {
+     console.log(response);
+     var qtdCart = parseInt($("span[data-cart=amount]").html());
+     $("span[data-cart=amount]").html(qtdCart + 1);
+   }).fail(function( jqXHR, status, errorThrown ){
+     var response = $.parseJSON( jqXHR.responseText );
+     console.log(response);
+   });
+ }
